b519e25ed0c5b3da463696d811e99058a439a0aa
Lei Wang <tendant@gmail.com>
2019-08-30 13:55:40 -0700

    update libraries

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [selmer "1.11.8"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [selmer "1.12.14"]
                  [selmer-test/child "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot parent.core
   :target-path "target/%s"
